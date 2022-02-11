c4ede14c53e696901bd8e71d29ebe7c6f384c4ff
tushar <tusharshitre123@gmail.com>
2022-02-11 18:11:37 +0530

    add valide last number

--- RegistrationRegex/RegistrationRegex/RegistrationValidation.cs
+++ RegistrationRegex/RegistrationRegex/RegistrationValidation.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Text.RegularExpressions;
 namespace UserRegistration
 {
     internal class RegistrationValidation
     {
         public void Checkfirstname(string firstname)
         {
-            string Firstnamepattern = "^[A-Z][a-z]{2}$";
+            string Firstnamepattern = "^[A-Z][a-z]{3,10}$";
             if (Regex.IsMatch(firstname, Firstnamepattern))
                 Console.WriteLine("First Name is valid");
+            else
+                Console.WriteLine("Invalid name!!");
+        }
+        public void Checklastname(string Lastname)
+        {
+            string lastnamepattern = "^[A-Z][a-z]{3,10}$";
+            if (Regex.IsMatch(Lastname, lastnamepattern))
+                Console.WriteLine("Last Name is valid");
             else
                 Console.WriteLine("Invalid name!!");
         }
