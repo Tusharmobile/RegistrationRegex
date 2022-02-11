92030961d107d0635112750cae21bc387b327dba
tushar <tusharshitre123@gmail.com>
2022-02-11 18:48:59 +0530

    add clear email program

--- RegistrationRegex/RegistrationRegex/RegistrationValidation.cs
+++ RegistrationRegex/RegistrationRegex/RegistrationValidation.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
+
 namespace UserRegistration
 {
     internal class RegistrationValidation
@@ -27,11 +28,12 @@ namespace UserRegistration
         }
         public void Checkemail(string Emailid)
         {
-            string emailpattern = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            // string emailpattern = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
+            string emailpattern = "^[a-zA-Z0-9]+[._+-]{0,1}[0-9a-zA-Z]+[@][0-9a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
             if (Regex.IsMatch(Emailid, emailpattern))
                 Console.WriteLine("Email id is valid");
             else
-                Console.WriteLine("Invalid name!!");
+                Console.WriteLine("Invalid Email id!!");
         }
         public void Checkmobilenum(string Mobilenum)
         {
@@ -43,11 +45,11 @@ namespace UserRegistration
         }
         public void Checkpassward(string Passward)
         {
-            string passwardpattern = "^[A-Za-z0-9]{8,20}$";
-            string passwardpattern1 = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
-            string passwardpattern2 = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
-            string passwardpattern3 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
-            if (Regex.IsMatch(Passward, passwardpattern)) 
+            // string passwardpattern = "^[A-Za-z0-9]{8,20}$";
+            // string passwardpattern1 = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
+            //string passwardpattern2 = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
+            if (Regex.IsMatch(Passward, passwardpattern))
                 Console.WriteLine("Passward is valid");
             else
                 Console.WriteLine("Invalid passward!!");
