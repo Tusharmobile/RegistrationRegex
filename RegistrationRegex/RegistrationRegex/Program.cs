701174fb53baf468c61cbf01bb38484d4d859767
tushar <tusharshitre123@gmail.com>
2022-02-11 13:01:18 +0530

    add valide first number

--- RegistrationRegex/RegistrationRegex/Program.cs
+++ RegistrationRegex/RegistrationRegex/Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RegistrationRegex
+namespace UserRegistration
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to User Registration");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter First name :");
+                        string fname = Console.ReadLine();
+                        RegistrationValidation Checkpattern = new RegistrationValidation();
+                        Checkpattern.Checkfirstname(fname);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("invalid option");
+                        break;
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
